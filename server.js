ff397ac22c595469321d731f688554b5f361cd75
TamilarasanG17 <tamilarasang17@gmail.com>
2025-08-12 19:12:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors)
+app.use(cors())
 app.use(express.static('public'));
 
 // Database Connection
